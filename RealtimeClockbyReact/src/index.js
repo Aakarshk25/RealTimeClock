cbd71012ada4b399725957ea35e527bfc99e5149
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2024-06-12 23:54:26 +0530

    Update index.js

--- RealtimeClockbyReact/src/index.js
+++ RealtimeClockbyReact/src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//Challenge:
+//Challenge: hiw
 //1. Given that you can get the current time using:
 
 //Show the latest time in the <h1> when the Get Time button
